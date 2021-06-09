865a8b5aa43e347803882f93f3c3d1b0e44c2314
minju960 <jmj2358@gmail.com>
2021-06-09 20:55:52 +0900

    Minju_School

--- student_management/src/main/java/com/mycom/student_management/controller/UserJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mycom.student_management.controller;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserJpaRepository extends JpaRepository<User, String> {
-	User findByUsername(String username);
-}
